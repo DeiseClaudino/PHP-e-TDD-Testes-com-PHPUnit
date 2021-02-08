4bb7ec619baf41dd487d952eb10933e24a367be1
DeiseClaudino <deise.claudino98@gmail.com>
2021-02-08 20:56:04 -0300

    Testes automatizados

--- teste-avaliador.php
+++ teste-avaliador.php
@@ -1,22 +1,33 @@
 <?php
 
+use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
-use Alura\Leilao\Model\Service\Avaliador;
 use Alura\Leilao\Model\Usuario;
+use Alura\Leilao\Model\Service\Avaliador;
 
 require 'vendor/autoload.php';
 
+// Arrange - Given / Preparamos o cenário do teste
 $leilao = new Leilao('Fiat 147 0km');
-
 
 $maria = new Usuario('Maria');
 $joao = new Usuario('Joao');
 
-$leilao->recebeLance(new \Alura\Leilao\Model\Lance($joao, 2000));
-$leilao->recebeLance(new \Alura\Leilao\Model\Lance($maria, 2500));
+$leilao->recebeLance(new Lance($joao, 2000));
+$leilao->recebeLance(new Lance($maria, 2500));
 
 $leiloeiro = new Avaliador();
+
+// Act - When / Executamos o código a ser testado
 $leiloeiro->avalia($leilao);
+
 $maiorValor = $leiloeiro->getMaiorValor();
 
-echo $maiorValor;
+// Assert - Then / Verificamos se a saída é a esperada
+$valorEsperado = 2500;
+
+if ($maiorValor == $valorEsperado) {
+    echo "TESTE OK";
+} else {
+    echo "TESTE FALHOU";
+}
