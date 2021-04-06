76cacba4b77824dd2ab291114b42713f71e6f750
DeiseClaudino <deise.claudino98@gmail.com>
2021-04-06 20:34:55 -0300

    Mais de cinco lances por usuário

--- src/Model/Leilao.php
+++ src/Model/Leilao.php
@@ -22,7 +22,33 @@ class Leilao
             return;
         }
 
+        $totalLancesUsuario = $this->quantidadeLancesPorUsuario($lance->getUsuario());
+
+        if ($totalLancesUsuario >= 5) 
+        {
+            return; 
+        }
+    
         $this->lances[] = $lance;
+    }
+
+    /**
+    * @param Usuario $usuario
+    * @return Lance|mixed
+    */
+    private function quantidadeLancesPorUsuario(Usuario $usuario): int
+    {
+        $totalLancesUsuario = array_reduce(
+            $this->lances,
+            function (int $totalAcumulado, Lance $lanceAtual) use ($usuario) {
+                if ($lanceAtual->getUsuario() == $usuario) {
+                    return $totalAcumulado + 1;
+                }
+                return $totalAcumulado;
+            },
+            0
+        );
+        return $totalLancesUsuario;
     }
     
     /**
